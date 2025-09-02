7db1cae1ad63bfcc550b2aa735c5ad40080410e0
J <jbohn4.jbk@gmail.com>
2025-09-02 11:41:46 +1000

    Update webrequests-randomgen.ps1

--- webrequests-randomgen.ps1
+++ webrequests-randomgen.ps1
@@ -1,5 +1,5 @@
 while ($true) {
-Invoke-WebRequest "shenanigans-$(-join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})).com": Start-Sleep 3 }
+Invoke-WebRequest "shenanigans-$(-join ((65..90) + (97..122) | Get-Random -Count 10 | % {[char]$_})).com"; Start-Sleep 3 }
 
 
 #could also be modified to use randomised TLDs but for now this just appends .com
